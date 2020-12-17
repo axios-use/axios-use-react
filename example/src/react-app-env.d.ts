f92797f89957bac1021173360433b159c2ad6b5f
wangcch <wangcch.cc@gmail.com>
2020-12-18 02:50:24 +0800

    docs: del example

--- example/src/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react-scripts" />
-
-interface Window {
-  __REDUX_DEVTOOLS_EXTENSION__:? () => any
-}
--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
 import url from 'rollup-plugin-url';
-import copy from 'rollup-plugin-cpy';
 import svgr from '@svgr/rollup';
 import pkg from './package.json';
 
@@ -34,25 +33,5 @@ export default {
       exclude: ['*.d.ts', '**/*.d.ts'],
     }),
     commonjs(),
-    copy([
-      // The example uses create-react-app (via create-react-library), which
-      // doesn't work correctly with yarn or npm links. It will end up with
-      // two versions of React in the build, which breaks hooks in particular
-      // since they rely on global state. To avoid this problem we simply copy
-      // the source directly into the example project.
-      //
-      // For more info about the issue:
-      // https://stackoverflow.com/questions/31169760/how-to-avoid-react-loading-twice-with-webpack-when-developing
-      {
-        files: [
-          'src/index.ts',
-          'src/request.ts',
-          'src/useRequest.ts',
-          'src/useResource.ts',
-          'src/requestContext.ts',
-        ],
-        dest: 'example/src/react-request-hook/',
-      },
-    ]),
   ],
 };
