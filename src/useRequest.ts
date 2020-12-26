f8f6d7511da7aaf256ef869d6ed87379d74ce846
wangcch <wangcch.cc@gmail.com>
2020-12-26 15:04:32 +0800

    refactor: error

--- src/useRequest.ts
+++ src/useRequest.ts
@@ -16,6 +16,9 @@ import { RequestContext } from "./requestContext";
 
 import { useMountedState } from "./utils";
 
+const REQUEST_AXIOS_INSTANCE_MESSAGE =
+  "react-request-hook requires an Axios instance to be passed through context via the <RequestProvider>";
+
 export type UseRequestResult<TRequest extends Request> = [
   {
     hasPending: boolean;
@@ -24,22 +27,13 @@ export type UseRequestResult<TRequest extends Request> = [
   RequestFactory<TRequest>,
 ];
 
-class MissingProviderError extends Error {
-  constructor() {
-    super(
-      "react-request-hook requires an Axios instance to be passed through " +
-        "context via the <RequestProvider>",
-    );
-  }
-}
-
 export function useRequest<TRequest extends Request>(
   fn: TRequest,
 ): UseRequestResult<TRequest> {
   const getMountedState = useMountedState();
   const axiosInstance = useContext(RequestContext);
   if (!axiosInstance) {
-    throw new MissingProviderError();
+    throw new Error(REQUEST_AXIOS_INSTANCE_MESSAGE);
   }
 
   const [sources, setSources] = useState<CancelTokenSource[]>([]);
