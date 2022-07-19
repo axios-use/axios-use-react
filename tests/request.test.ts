73c64fe04a4b3e07f66b292f16e1169faa1f6f67
cicheng <cicheng.wang@wework.cn>
2022-07-19 15:59:44 +0800

    test(request): promise returns

--- tests/request.test.ts
+++ tests/request.test.ts
@@ -1,6 +1,8 @@
 import type { AxiosRequestConfig } from "axios";
 import { request, createRequestError } from "../src";
 
+import { mockAdapter, axios } from "./utils";
+
 const config1 = { url: "/config1", method: "GET" } as AxiosRequestConfig;
 const config2 = {
   url: "/config2",
@@ -55,5 +57,53 @@ describe("createRequestError", () => {
     expect(createRequestError(undefined as any).code).toBeUndefined();
     expect(createRequestError(undefined as any).data).toBeUndefined();
     expect(createRequestError(undefined as any).original).toBeUndefined();
+  });
+});
+
+describe("request promise", () => {
+  beforeAll(() => {
+    mockAdapter.onGet("/gethello").reply((config) => {
+      if (config.headers?.["xxxkey"] === "custom") {
+        return [200, "custom"];
+      }
+      return [200, "hello"];
+    });
+    mockAdapter.onPost("/post").reply((config) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      return [200, { data: JSON.parse(config.data) }];
+    });
+  });
+
+  it("instance true", async () => {
+    const res = await request<string>({ url: "/gethello" }, true);
+
+    expect(res.data).toBe("hello");
+  });
+
+  it("instance custom", async () => {
+    const instance = axios.create({
+      headers: {
+        xxxkey: "custom",
+      },
+    });
+    const res = await request<string>({ url: "/gethello" }, instance);
+
+    expect(res.data).toBe("custom");
+  });
+
+  it("check response type", async () => {
+    type Res = {
+      data?: {
+        demo: string;
+      };
+    };
+    const body: Res["data"] = { demo: "hello" };
+
+    const res = await request<Res>(
+      { method: "post", url: "/post", data: body },
+      true,
+    );
+
+    expect(res.data?.data?.demo).toBe("hello");
   });
 });
